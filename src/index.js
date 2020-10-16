493a7c121ed3df648e2a67c6a3e87e4a17b104b9
Joaquín Buendía <jgamer669@gmail.com>
2020-10-16 19:02:17 +0200

    "Website Archives"

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const indexRoutes = require('./routes/index');
+
+//Setings
+app.set('port',3000);
+app.set('views',path.join(__dirname , 'views'))
+app.engine('html',require('ejs').renderFile)
+app.set('view engine','ejs');
+
+
+//Routes
+app.use(indexRoutes);
+
+
+//Static Files
+app.use(express.static(path.join(__dirname,'public')))
+
+//Middleware
+
+
+//Listening
+app.listen(app.get('port'),(req,res)=>{
+    console.log('Server on port', app.get('port'));
+});
