420c54e813eada147417f13861b502c5eb1e8d3d
Joaquín Buendía <jgamer669@gmail.com>
2020-10-16 19:29:47 +0200

    ""

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const indexRoutes = require('./routes/index');
 
 //Setings
-app.set('port',3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views',path.join(__dirname , 'views'))
 app.engine('html',require('ejs').renderFile)
 app.set('view engine','ejs');
